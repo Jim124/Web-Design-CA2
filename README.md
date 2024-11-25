cdbeb03ae86545b467b982772663b0b0fcfbbdcc
Lu <stupidlifewww@foxmail.com>
2024-11-25 13:48:02 +0000

    template navbar n footer

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
 # Web-Design-CA2
+
+## Team: myHero
+
 web design
+
+
+## Author
+- **Huini Lu** 
+
+## Date
+- 25/11/2024
+
+
+## Table of Contents
+- [Starting a Local Server](#starting-a-local-server)
+- [Reusing navbar.html and footer.html](#reusing-navbarhtml-and-footerhtml)
+- [Replacing the Web Name and Links](#replacing-the-web-name-and-links)
+
+## Starting a Local Server
+- enter the folder where your project is located
+	- `cd /path/to/your_project`
+- execute commands `python3 -m http.server` in the terminal (command line)
+- check the open port
+	- e.g., `Serving HTTP on :: port 8000 (http://[::]:8000/) ...`
+	- usually, the address is `http://localhost:8000`
+
+## Reusing `navbar.html`, `footer.html`
+- import Google font
+- import custom css `css/template.css`
+- import `bootstrap` 
+- import `Jquery`
+	- import `<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>` in the `<head> </head>`
+	- make sure there is `js/templateScript.js` in your project
+	- import `<script src="js/templateScript.js"></script>`
+	,and add this line before the closing body tag
+- include a placeholder div in your html where you want
+      <div id="navbar-placeholder"></div>
+      ```
+    - Footer placeholder:
+      ```html
+      <div id="footer-placeholder"></div>
+      ```
+	- `stay.html` is provided as an example of how to use `navbar.html` and `footer.html`. Refer to it if you need guidance on implementation.
+
+## Replacing the Web Name and Links
+- enter `js` folder
+- open `templateScript.js` file
+- replace the default address or name with yours in the lists
+	- navMenuItems
+	```js
+	const navMenuItems = [
+	  { name: "Home", href: "index.html" },
+	  { name: "Stay", href: "stay.html" },
+	  { name: "Food & Drinks", href: "#" },
+	  { name: "Outdoor", href: "#" },
+	  { name: "Festival & Events", href: "#" }
+	];
+	```
+	- footerMenuItems
+	```js
+	  // footer menu list
+	 const footerMenuItems = [
+	  { name: "Home", href: "index.html" },
+	  { name: "Where to stay", href: "stay.html"},
+	  { name: "Food & Drinks", href: "#"},
+	  { name: "Outdoor", href: "#"},
+	  { name: "Festival & Events", href: "#"}
+	  ];
+	// footer legal list
+	 const footerLegalItems = [
+	  { name: "Terms&conditions"},
+	  { name: "Accessibility"},
+	  { name: "Cookie policy"},
+	  { name: "Privacy Statement"}
+	  ];
+	```
---
+++ js/templateScript.js
@@ -0,0 +1,127 @@
+
+$(document).ready(function () {
+  // navbar menu list
+ const navMenuItems = [
+  { name: "Home", href: "index.html" },
+  { name: "Stay", href: "stay.html"},
+  { name: "Food&Drinks", href: "#"},
+  { name: "Outdoor", href: "#"},
+  { name: "Festival&Events", href: "#"}
+  ];
+  // footer menu list
+ const footerMenuItems = [
+  { name: "Home", href: "index.html" },
+  { name: "Where to stay", href: "stay.html"},
+  { name: "Food & Drinks", href: "#"},
+  { name: "Outdoor", href: "#"},
+  { name: "Festival & Events", href: "#"}
+  ];
+// footer legal list
+ const footerLegalItems = [
+  { name: "Terms&conditions"},
+  { name: "Accessibility"},
+  { name: "Cookie policy"},
+  { name: "Privacy Statement"}
+  ];
+
+// extract code for reuse navbar n footer
+ $("#navbar-placeholder").load("navbar.html", function () {
+  console.log("Navbar loaded");
+  setNavActiveLink();
+});
+
+ $("#footer-placeholder").load("footer.html", function () {
+  console.log("Footer loaded");
+  setFooterActiveLink();
+});
+
+  // toggle tabs
+  // match current page link
+ function setNavActiveLink() {
+  const currentPath = window.location.pathname.split("/").pop() || "index.html";
+
+  // Dynamically generate footer sub menu list
+  $.each(navMenuItems, function (index, item) {
+    $(".navbar-nav").append(`
+      <li class="nav-item me-2">
+      <a class="nav-link" href="${item.href}">
+      <span>${item.name}</span>
+      </a>
+      </li>
+      `);
+  });
+
+  $(".nav-link").each(function () {
+    if ($(this).attr("href") === currentPath) {
+      $(this).addClass("active");
+    } else {
+      $(this).removeClass("active");
+    }
+  });
+
+  // onclick, change active class
+  $(".nav-link").on("click", function () {
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+  });
+}
+function setFooterActiveLink() {
+  // get actived page
+  const currentPath = window.location.pathname.split("/").pop() || "index.html";
+
+  // Dynamically generate footer sub menu list
+  $.each(footerMenuItems, function (index, item) {
+    $(".footer-sub-link").append(`
+      <li class="footer-item">
+      <a class="footer-link" href="${item.href}">${item.name}</a>
+      </li>
+      `);
+  });
+
+  // Dynamically generate footer legal list
+  $.each(footerLegalItems, function (index, item) {
+    $(".footer-legal").append(`
+      <li class="footer-item">
+      <span class="footer-link">${item.name}</span>
+      </li>
+      `);
+  });
+
+  // handle active class 
+  $(".footer-link").each(function () {
+    if ($(this).attr("href") === currentPath) {
+      $(this).addClass("active");
+    } else {
+      $(this).removeClass("active");
+    }
+  });
+
+  $(".footer-link").on("click", function () {
+    $(".footer-link").removeClass("active");
+    $(this).addClass("active");
+  });
+}
+});
+
+// scroll navbar
+$(document).ready(function () {
+    // save the postion where last time scrolled
+  let lastScrollTop = 0; 
+
+  $(window).on("scroll", function () {
+      // get the current scroll sposition
+    let currentScrollTop = $(this).scrollTop();
+
+    if (currentScrollTop > 100 && currentScrollTop > lastScrollTop) {
+        // scroll down, hidden nabar
+      $(".navbar").removeClass("navbar-fixed").addClass("navbar-hidden");
+    } else {
+        // scroll top, fix navbar
+      $(".navbar").removeClass("navbar-hidden").addClass("navbar-fixed");
+    }
+      // update scroll position
+    lastScrollTop = currentScrollTop;
+  });
+});
+
+
