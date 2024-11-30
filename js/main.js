b0f04d0fd752e533a28a1ac143f812ce6a2ec5a5
Jim124 <fengpingan065@gmail.com>
2024-11-30 13:06:30 +0000

    modify main.js (add festival.html)

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     { name: 'Where to stay', href: 'live.html' },
     { name: 'Food & Drinks', href: 'food_drinks.html' },
     { name: 'Outdoor', href: 'outdoor.html' },
-    { name: 'Festival & Events', href: '#' },
+    { name: 'Festival & Events', href: 'festival.html' },
   ];
   // footer legal list
   const footerLegalItems = [
