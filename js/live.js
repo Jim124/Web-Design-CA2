a6989d17813c68277db70c948ccea867b816f932
Jim124 <fengpingan065@gmail.com>
2024-12-04 11:41:43 +0000

    modify live.js

--- js/live.js
+++ js/live.js
@@ -19,4 +19,13 @@ $(document).ready(function () {
     //   $('#hotel-description').css('display', 'none');
     // }
   });
+
+  $('#warehouse').on('click', function () {
+    $('#stay-img').attr('src', 'src/images/caravan-1.jpg');
+    $('#hotel-description').css('display', 'block');
+  });
+  $('#cottage').on('click', function () {
+    $('#stay-img').attr('src', 'src/images/caravan-2.jpg');
+    $('#hotel-description').css('display', 'block');
+  });
 });
