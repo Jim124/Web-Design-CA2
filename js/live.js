5674e9b6d3e8d1dc2f59af5c051c91fec33e9fd3
Jim124 <fengpingan065@gmail.com>
2024-12-05 17:03:46 +0000

    modify live.js

--- js/live.js
+++ js/live.js
@@ -1,19 +1,23 @@
 $(document).ready(function () {
   $('#hotel').on('click', function () {
     $('#stay-img').attr('src', 'src/images/hotel.jpg');
+    $('#stay-img').attr('alt', 'hotel');
     $('#hotel-description').css('display', 'block');
   });
   $('#cabin').on('click', function () {
     $('#stay-img').attr('src', 'src/images/cabin-5.jpg');
+    $('#stay-img').attr('alt', 'cabin');
     $('#hotel-description').css('display', 'block');
   });
 
   $('#warehouse').on('click', function () {
     $('#stay-img').attr('src', 'src/images/caravan-1.jpg');
+    $('#stay-img').attr('alt', 'house');
     $('#hotel-description').css('display', 'block');
   });
   $('#cottage').on('click', function () {
     $('#stay-img').attr('src', 'src/images/caravan-2.jpg');
+    $('#stay-img').attr('alt', 'cottage');
     $('#hotel-description').css('display', 'block');
   });
 });
