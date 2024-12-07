974f775e2046021a0d20812ff3d982603b8890aa
MyheroGunji <gmpoo93.982@gmail.com>
2024-12-07 06:26:17 +0000

    modify

--- js/script_mahiro.js
+++ js/script_mahiro.js
@@ -19,6 +19,24 @@ document.addEventListener("DOMContentLoaded", () => {
   });
   
   //outdoor-section2
+  document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("#concept-two");
+  
+    const observerOptions = {
+      threshold: 0.4
+    };
+  
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fade-in");
+          observer.unobserve(entry.target);
+        }
+      });
+    }, observerOptions);
+  
+    sections.forEach(section => observer.observe(section));
+  });
   
   //outdoor-section3
   window.addEventListener('scroll', function() {
@@ -36,13 +54,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
   
-  //Testimonial JS
+  //Cafe Testimonial 
   $(document).ready(function(){
     $(".testimonial-carousel").owlCarousel({
       items: 4,
       margin: 30,
       loop: true,
+      nav: false,         
+      dots: false, 
       autoplay: true,
+      autoplayTimeout: 3000, // each 3 seconds
+      autoplayHoverPause: true, 
       responsive: {
         0: { items: 1 },
         768: { items: 2 },
@@ -50,15 +72,27 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   });
-  
-  
-  // alart
-  document.addEventListener("DOMContentLoaded", () => {
-      const button = document.querySelector(".main-border-button a");
-  
-      button.addEventListener("click", (event) => {
-          event.preventDefault(); 
-          alert("Enjoy descovoring Dublin!");
-      });
-  });
 
+  document.addEventListener("DOMContentLoaded", () => {
+    // 特定のボタン（.special-button）を選択
+    const button = document.querySelector(".special-button");
+  
+    // イベントリスナーを追加
+    if (button) {
+      button.addEventListener("click", (event) => {
+        event.preventDefault();
+        alert("Enjoy discovering Dublin!");
+      });
+    }
+  });
+  
+
+// Back to top button : show up if user scroll up to 300px
+window.addEventListener('scroll', function() {
+  const backToTopButton = document.querySelector('.back-to-top');
+  if (window.scrollY > 300) { 
+    backToTopButton.classList.remove('hidden'); 
+  } else {
+    backToTopButton.classList.add('hidden'); 
+  }
+});
